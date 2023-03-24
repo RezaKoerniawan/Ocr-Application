fa5ee054b22de9cd47376f93ed026e15b20acb6b
Reza Kurniawan <reza.koerniawan99@gmail.com>
2023-03-24 15:20:39 +0700

    add : permission camera and location

--- app/src/main/java/com/reza/ocrapplication/FirstFragment.kt
+++ app/src/main/java/com/reza/ocrapplication/FirstFragment.kt
@@ -1,12 +1,18 @@
 package com.reza.ocrapplication
 
+import android.Manifest
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.reza.ocrapplication.databinding.FragmentFirstBinding
+import com.reza.ocrapplication.utils.PermissionResultListener
+import com.reza.ocrapplication.utils.RequestPermissionListener
+import com.reza.ocrapplication.utils.handleOnRequestPermissionResult
+import com.reza.ocrapplication.utils.requestPermissions
+import com.reza.ocrapplication.utils.showDialog
 
 class FirstFragment : Fragment() {
 
@@ -30,12 +36,86 @@ class FirstFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            requestMultiplePermissionWithListener()
+            //findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    // multiple permissions
+    private val multiplePermissions = arrayOf(
+        Manifest.permission.CAMERA,
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.ACCESS_FINE_LOCATION,
+    )
+    private val multiplePermissionsCode = 2111
+
+    private fun requestMultiplePermissionWithListener() {
+        requestPermissions(
+            multiplePermissions,
+            multiplePermissionsCode,
+            object : RequestPermissionListener {
+                override fun onPermissionRationaleShouldBeShown(requestPermission: () -> Unit) {
+                    showDialog(
+                        message = "Please allow permissions to use this feature",
+                        textPositive = "OK",
+                        positiveListener = {
+                            requestPermission.invoke()
+                        },
+                        textNegative = "Cancel"
+                    )
+                }
+
+                override fun onPermissionPermanentlyDenied(openAppSetting: () -> Unit) {
+                    showDialog(
+                        message = "Permission Disabled, Please allow permissions to use this feature",
+                        textPositive = "OK",
+                        positiveListener = {
+                            openAppSetting.invoke()
+                        },
+                        textNegative = "Cancel"
+                    )
+                }
+
+                override fun onPermissionGranted() {
+                    showToast("Granted, do work")
+                }
+            })
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        // multiple permission
+        handleOnRequestPermissionResult(
+            multiplePermissionsCode,
+            requestCode,
+            permissions,
+            grantResults,
+            object : PermissionResultListener {
+                override fun onPermissionRationaleShouldBeShown() {
+                    showToast("permission denied")
+                }
+
+                override fun onPermissionPermanentlyDenied() {
+                    showToast("permission permanently disabled")
+                }
+
+                override fun onPermissionGranted() {
+                    showToast("permission granted")
+                }
+            }
+        )
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(context, "Fragment $message", Toast.LENGTH_SHORT).show()
     }
 }
